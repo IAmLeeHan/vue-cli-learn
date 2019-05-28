5b44e969ea629f7cd239b52f8292205c6a3ea9e7
IAMLeeHan <lihan199301@163.com>
2019-05-28 17:26:22 +0800

    vue.cli项目搭建及文件的作用

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="app">
+    <a href="">hello world!!!</a>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang = "scss">
+
+</style>
+
+<script>
+  // 引入hello world组件
+  import HelloWorld from "@/components/HelloWorld"//@符号指的是当前src路径
+  export default {
+    components:{//注册子组件
+      HelloWorld
     }
   }
-}
-</style>
+</script>
